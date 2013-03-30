824e14781c14077c41a70182373633bd450b11be
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-03-30 12:49:21 +0900

    fix inheritations.  add head filter

--- night.js
+++ night.js
@@ -1,16 +1,18 @@
 // vim:set ts=8 sts=2 sw=2 tw=0:
 
 var fs = require('fs');
-var Reader = require('./lib/stream_line_reader').StreamLineReader;
+var Reader = require('./lib/stream_line_reader');
+var Head = require('./lib/head_filter');
 
 var s = fs.createReadStream('test.txt', { bufferSize: 1 });
-var r = new Reader(s);
+var reader = new Reader(s);
+var head = new Head(reader);
 
 var count = 0;
-r.on('line', function(line) {
+head.on('line', function(line) {
     console.log('line: ' + line);
     ++count;
 });
-r.on('end', function() {
+head.on('end', function() {
     console.log('count: ' + count);
 });
